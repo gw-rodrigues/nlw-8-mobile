a5fa0929079bfeb844f37b5f274f1ff704f0599b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-10 00:11:18 +0200

    Add bottomSheet, Gesturehadler, func open widget

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef } from "react";
+import { TouchableOpacity } from "react-native";
+import { ChatTeardropDots } from "phosphor-react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { theme } from "../../theme";
 
-export function Widget() {
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand()
+  }
+
   return (
-    <View style={styles.container}>
-        <Text>Hello world!</Text>
-    </View>
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpen} >
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1,280]}
+      >
+
+      </BottomSheet>
+      
+    </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget)
