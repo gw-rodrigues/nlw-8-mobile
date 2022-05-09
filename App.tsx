b62131441c9c8e49ea832b52eda9b0a2a3dc003e
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-10 00:00:47 +0200

    re-ordering

--- App.tsx
+++ App.tsx
@@ -1,34 +1,40 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import AppLoading from 'expo-app-loading';
-import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import "react-native-gesture-handler";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 
-import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import { theme } from "./src/theme";
+import { Widget } from "./src/components/Widget";
 
 export default function App() {
-  
   let [fontsLoaded] = useFonts({
-    Inter_400Regular, Inter_500Medium
+    Inter_400Regular,
+    Inter_500Medium,
   });
 
   if (!fontsLoaded) {
-    return (<><AppLoading /></>)
+    return (
+      <>
+        <AppLoading />
+      </>
+    );
   }
 
   return (
-    <View style={{
-      flex:1,
-      backgroundColor: theme.colors.background
-    }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <StatusBar style="light" backgroundColor="transparent" translucent />
       <Widget/>
-      <StatusBar 
-        style="light" 
-        backgroundColor='transparent'
-        translucent
-      />
     </View>
   );
 }
