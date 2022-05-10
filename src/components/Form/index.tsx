7257cf644715d406c3de893eab814a5c2f684914
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-11 00:15:10 +0200

    Sharing feedback and functions

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import { ArrowLeft } from "phosphor-react-native";
-import { captureScreen } from 'react-native-view-shot'
+import { captureScreen } from "react-native-view-shot";
 
 import { FeedbackType } from "../Widget";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -13,31 +13,49 @@ import { theme } from "../../theme";
 
 interface FormProps {
   feedbackType: FeedbackType;
+  onFeedbackCanceled: () => void;
+  onFeedbackSent: () => void;
 }
 
-export function Form({ feedbackType }: FormProps) {
-
-  const [screenshot, setScreenshot] = useState<string | null>(null)
+export function Form({
+  feedbackType,
+  onFeedbackCanceled,
+  onFeedbackSent,
+}: FormProps) {
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+  const [screenshot, setScreenshot] = useState<string | null>(null);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleScreenshot(){
+  function handleScreenshot() {
     captureScreen({
-      format:'jpg',
-      quality:0.8
+      format: "jpg",
+      quality: 0.8,
     })
-    .then(uri => setScreenshot(uri))
-    .catch(error => console.log(error))
+      .then((uri) => setScreenshot(uri))
+      .catch((error) => console.log(error));
   }
 
-  function handleScreenshotRemove(){
-    setScreenshot(null)
+  function handleScreenshotRemove() {
+    setScreenshot(null);
+  }
+
+  async function handleSendFeedback() {
+    if (isSendingFeedback) {
+      return;
+    }
+    setIsSendingFeedback(true);
+    try{
+
+    }catch(error){
+      console.log(error)
+    }
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={onFeedbackCanceled}>
           <ArrowLeft
             size={24}
             weight="bold"
@@ -54,6 +72,7 @@ export function Form({ feedbackType }: FormProps) {
         style={styles.input}
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         placeholderTextColor={theme.colors.text_secondary}
+        autoCorrect={false}
       />
 
       <View style={styles.footer}>
@@ -62,7 +81,7 @@ export function Form({ feedbackType }: FormProps) {
           onRemoveShot={handleScreenshotRemove}
           screenshot={screenshot}
         />
-        <Button isLoading={false} />
+        <Button onPress={handleSendFeedback} isLoading={isSendingFeedback} />
       </View>
     </View>
   );
--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Copyright } from '../Copyright';
-import { Option } from '../Option';
+import React from "react";
+import { View, Text } from "react-native";
+import { Copyright } from "../Copyright";
+import { Option } from "../Option";
 
-import { feedbackTypes } from '../../utils/feedbackTypes';
+import { feedbackTypes } from "../../utils/feedbackTypes";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { FeedbackType } from "../Widget";
 
-export function Options() {
+interface OptionsProps {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+export function Options({ onFeedbackTypeChanged }: OptionsProps) {
   return (
     <View style={styles.container}>
-
-      <Text style={styles.title}>
-        Deixe seu feedback
-      </Text>
+      <Text style={styles.title}>Deixe seu feedback</Text>
 
       <View style={styles.options}>
-        {
-          Object.entries(feedbackTypes)
-          .map(([key,value])=>(
-            <Option 
-              key={key} 
-              title={value.title}
-              image={value.image}
-            />
-          ))
-        }
+        {Object.entries(feedbackTypes).map(([key, value]) => (
+          <Option
+            key={key}
+            title={value.title}
+            image={value.image}
+            onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
+          />
+        ))}
       </View>
-        <Copyright/>
+      <Copyright />
     </View>
   );
 }
